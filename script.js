a5badfcee4bbf61b316ff9a2fdf4b4bc06d1d953
Katerina Orrava <98848233+KateRiinaO@users.noreply.github.com>
2022-10-04 09:59:10 +0300

    Making circles active

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 const progress=document.getElementById('progress')
-const prec=document.getElementById('prev')
-const next=documment.getElementById('next')
+const prev=document.getElementById('prev')
+const next=document.getElementById('next')
 const circles=document.querySelectorAll('.circle')
+
+
+//
+let currentActive=1
+
+next.addEventListener('click', ()=> {
+    //adds +1 for the currentActive
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive=circles.length
+    }
+
+    update()
+})
+
+prev.addEventListener('click', ()=> {
+    //substracts -1 for the currentActive
+    currentActive--
+
+    if(currentActive <1) {
+        currentActive=1
+    }
+
+    update()
+})
+
+function update() {
+    //looping through circles to make them non-/active
+    circles.forEach((circle, idx) => {
+        if(idx<currentActive) {
+            circle.classList.add('active')
+        } else {
+    circle.classList.remove('active')
+        } 
+    })
+}
