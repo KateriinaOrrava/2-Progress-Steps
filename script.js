525b9cff80c2762bf6169f37c225385ea11d059a
Katerina Orrava <98848233+KateRiinaO@users.noreply.github.com>
2022-10-04 09:43:24 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const progress=document.getElementById('progress')
+const prec=document.getElementById('prev')
+const next=documment.getElementById('next')
+const circles=document.querySelectorAll('.circle')
